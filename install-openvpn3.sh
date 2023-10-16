49511558468272e1a0a4e3639e2abf887ffcc3ce
Joris Clement <joclement@posteo.net>
2023-10-16 14:00:05 +0200

    feat: install openvpn3

---
+++ install-openvpn3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+curl -fsSL https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub | gpg --dearmor > /tmp/openvpn-repo-pkg-keyring.gpg
+sudo mv /tmp/openvpn-repo-pkg-keyring.gpg /etc/apt/trusted.gpg.d/.
+curl -fsSL https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-jammy.list > /tmp/openvpn3.list
+sudo mv /tmp/openvpn3.list /etc/apt/sources.list.d/openvpn3.list
+sudo apt-get update
+sudo apt-get install -y openvpn3
