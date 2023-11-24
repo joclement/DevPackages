addf06c918a4ddf5290ee4de82fdd706eb247a23
Joris Clement <7713214+joclement@users.noreply.github.com>
2023-11-24 22:45:56 +0100

    refactor: reduce redundancy

--- install-gitlab-cli.sh
+++ install-gitlab-cli.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail
 
-curl -fsSL --output-dir /tmp -O "https://gitlab.com/gitlab-org/cli/-/releases/v1.34.0/downloads/glab_1.34.0_Linux_x86_64.deb"
-sudo apt-get install -f /tmp/glab_1.34.0_Linux_x86_64.deb
+GLAB_VERSION="1.34.0"
+
+curl -fsSL --output-dir /tmp -O "https://gitlab.com/gitlab-org/cli/-/releases/v${GLAB_VERSION}/downloads/glab_${GLAB_VERSION}_Linux_x86_64.deb"
+sudo apt-get install -f /tmp/glab_${GLAB_VERSION}_Linux_x86_64.deb
