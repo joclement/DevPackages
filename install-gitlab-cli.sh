efb77f56f96697d8c7f4645ff43b624d0884175a
Joris Clement <7713214+joclement@users.noreply.github.com>
2023-11-24 22:46:14 +0100

    feat: upgrade Gitlab cli tool

--- install-gitlab-cli.sh
+++ install-gitlab-cli.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-GLAB_VERSION="1.34.0"
+GLAB_VERSION="1.35.0"
 
 curl -fsSL --output-dir /tmp -O "https://gitlab.com/gitlab-org/cli/-/releases/v${GLAB_VERSION}/downloads/glab_${GLAB_VERSION}_Linux_x86_64.deb"
 sudo apt-get install -f /tmp/glab_${GLAB_VERSION}_Linux_x86_64.deb
