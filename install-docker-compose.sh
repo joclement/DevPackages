f22e2fe70bdfb3681a3dc88da5267ff21111e3f3
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-02-02 10:38:25 +0100

    refactor: prefer long, self-explaining options

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 
 VERSION="v2.23.3"
 
-sudo apt-get purge -y docker-compose-v2
+sudo apt-get purge --yes docker-compose-v2
 
-mkdir -p ~/.docker/cli-plugins/
-curl -fsSL https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-linux-x86_64 \
+mkdir --parents ~/.docker/cli-plugins/
+curl --fail --silent --show-error --location \
+     https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-linux-x86_64 \
      --output ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
