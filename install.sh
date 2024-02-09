fda1c4e5e5b4ed62a23fb2b0dfef2776918e6a7a
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-02-09 17:10:50 +0100

    build: autoformat bash scripts

--- install.sh
+++ install.sh
@@ -6,59 +6,65 @@ sudo apt-get purge -y docker
 
 sudo apt-get update
 sudo apt-get remove -y containerd.io || true
-sudo apt-get install -y apt-file \
-                        apt-transport-https \
-                        binutils \
-                        ccache \
-                        clang-15 \
-                        clang-format-15 \
-                        clang-tidy-15 \
-                        clangd-15 \
-                        cmake \
-                        curl \
-                        datamash \
-                        docker.io \
-                        fonts-hack \
-                        fzf \
-                        git-crypt \
-                        git-extras \
-                        git-lfs \
-                        htop \
-                        jq \
-                        libperl-critic-perl \
-                        ncal \
-                        net-tools \
-                        oathtool \
-                        pandoc \
-                        perl \
-                        python-is-python3 \
-                        python3-doc \
-                        python3-pip \
-                        python3-venv \
-                        shfmt \
-                        swaks \
-                        tig \
-                        tmux \
-                        traceroute \
-                        tree \
-                        universal-ctags \
-                        xclip \
-                        xmlstarlet \
-                        yamllint \
-                        zeal
+sudo apt-get install -y \
+    apt-file \
+    apt-transport-https \
+    binutils \
+    ccache \
+    clang-15 \
+    clang-format-15 \
+    clang-tidy-15 \
+    clangd-15 \
+    cmake \
+    curl \
+    datamash \
+    docker.io \
+    fonts-hack \
+    fzf \
+    git-crypt \
+    git-extras \
+    git-lfs \
+    htop \
+    jq \
+    libperl-critic-perl \
+    ncal \
+    net-tools \
+    oathtool \
+    pandoc \
+    perl \
+    python-is-python3 \
+    python3-doc \
+    python3-pip \
+    python3-venv \
+    shfmt \
+    swaks \
+    tig \
+    tmux \
+    traceroute \
+    tree \
+    universal-ctags \
+    xclip \
+    xmlstarlet \
+    yamllint \
+    zeal
 
 sudo apt-file update
 
-sudo update-alternatives --install /usr/bin/clang clang \
-                                   /usr/bin/clang-15 100
-sudo update-alternatives --install /usr/bin/clang++ clang++ \
-                                   /usr/bin/clang++-15 100
-sudo update-alternatives --install /usr/bin/clang-format clang-format \
-                                   /usr/bin/clang-format-15 100
-sudo update-alternatives --install /usr/bin/clang-tidy clang-tidy \
-                                   /usr/bin/clang-tidy-15 100
-sudo update-alternatives --install /usr/bin/clangd clangd \
-                                   /usr/bin/clangd-15 100
+sudo update-alternatives --install \
+    /usr/bin/clang clang \
+    /usr/bin/clang-15 100
+sudo update-alternatives --install \
+    /usr/bin/clang++ clang++ \
+    /usr/bin/clang++-15 100
+sudo update-alternatives --install \
+    /usr/bin/clang-format clang-format \
+    /usr/bin/clang-format-15 100
+sudo update-alternatives --install \
+    /usr/bin/clang-tidy clang-tidy \
+    /usr/bin/clang-tidy-15 100
+sudo update-alternatives --install \
+    /usr/bin/clangd clangd \
+    /usr/bin/clangd-15 100
 
 sudo usermod -a -G docker "$(whoami)"
 
@@ -73,7 +79,7 @@ pipx install --pip-args=--constraint=pipx_requirements.txt conan
 pipx install --pip-args=--constraint=pipx_requirements.txt cookiecutter
 pipx install --pip-args=--constraint=pipx_requirements.txt cruft
 pipx install --pip-args=--constraint=pipx_requirements.txt nox
-pipx inject  --pip-args=--constraint=pipx_requirements.txt nox nox-poetry
+pipx inject --pip-args=--constraint=pipx_requirements.txt nox nox-poetry
 pipx install --pip-args=--constraint=pipx_requirements.txt pdm
 pipx install --pip-args=--constraint=pipx_requirements.txt pip-tools
 pipx install --pip-args=--constraint=pipx_requirements.txt poetry
