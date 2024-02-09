91f73e9918cee2eced9a1b95c0f8969704a98482
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-02-09 17:08:31 +0100

    refactor: adapt code to obey line width

--- install-hadolint.sh
+++ install-hadolint.sh
@@ -2,9 +2,12 @@
 
 set -euo pipefail
 
+REPO=https://github.com/hadolint/hadolint
+
 VERSION="v2.12.0"
 
 mkdir -p ~/bin
-curl -fsSL https://github.com/hadolint/hadolint/releases/download/${VERSION}/hadolint-Linux-x86_64 \
-     --output ~/bin/hadolint
+curl --fail --silent --show-error --location \
+    $REPO/releases/download/${VERSION}/hadolint-Linux-x86_64 \
+    --output ~/bin/hadolint
 chmod +x ~/bin/hadolint
