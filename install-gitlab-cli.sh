4e9c89e94f298882322266604e9d770d0e55e91f
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-02-14 11:45:24 +0100

    refactor: shorten variable name

--- install-gitlab-cli.sh
+++ install-gitlab-cli.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-GLAB_VERSION="1.36.0"
+VERSION="1.36.0"
 
 GITLAB_URL=https://gitlab.com/gitlab-org/cli/-/releases
-GLAB_URL="$GITLAB_URL/v${GLAB_VERSION}/downloads/glab_${GLAB_VERSION}_Linux_x86_64.deb"
+GLAB_URL="$GITLAB_URL/v${VERSION}/downloads/glab_${VERSION}_Linux_x86_64.deb"
 
 curl --fail --silent --show-error --location \
     --output-dir /tmp \
     --remote-name "$GLAB_URL"
-sudo apt-get install -f /tmp/glab_${GLAB_VERSION}_Linux_x86_64.deb
+sudo apt-get install -f /tmp/glab_${VERSION}_Linux_x86_64.deb
