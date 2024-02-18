a894372f32d0e03d0be7212a99c49d1f19133efa
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-02-18 23:11:33 +0100

    build: get renovate to update hadolint

--- install-hadolint.sh
+++ install-hadolint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 REPO=https://github.com/hadolint/hadolint
 
-VERSION="v2.12.0"
+VERSION="2.12.0"
 
 if command -v hadolint > /dev/null; then
     VERSION_EXISTING=$(hadolint --version | grep -oE '[0-9|.]+')
@@ -17,6 +17,6 @@ fi
 
 mkdir -p ~/bin
 curl --fail --silent --show-error --location \
-    $REPO/releases/download/${VERSION}/hadolint-Linux-x86_64 \
+    $REPO/releases/download/v${VERSION}/hadolint-Linux-x86_64 \
     --output ~/bin/hadolint
 chmod +x ~/bin/hadolint
