2703fa26ad02c5349eed413a087a22eed0bc29bf
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-07 09:21:24 +0200

    fix: do not overwrite go executable
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-golang.sh
+++ install-golang.sh
@@ -9,5 +9,5 @@ sudo update-alternatives --install \
     /usr/local/bin/go go \
     /usr/lib/go-1.21/bin/go 100
 sudo update-alternatives --install \
-    /usr/local/bin/gofmt go \
+    /usr/local/bin/gofmt gofmt \
     /usr/lib/go-1.21/bin/gofmt 100
