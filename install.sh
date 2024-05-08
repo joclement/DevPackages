63bb91d63be3d6171890da4dce9b54566c2618e0
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-08 11:11:39 +0200

    refactor: allow to quickly install a new package locally
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -4,50 +4,7 @@ set -euo pipefail
 
 sudo apt-get update
 
-sudo apt-get install --yes \
-    apt-file \
-    apt-transport-https \
-    binutils \
-    build-essential \
-    ccache \
-    cmake \
-    curl \
-    datamash \
-    devscripts \
-    dos2unix \
-    fonts-hack \
-    fzf \
-    git \
-    git-crypt \
-    git-extras \
-    git-lfs \
-    htop \
-    jq \
-    libperl-critic-perl \
-    ncal \
-    net-tools \
-    oathtool \
-    pandoc \
-    parallel \
-    perl \
-    python-is-python3 \
-    python3 \
-    python3-doc \
-    python3-pip \
-    python3-venv \
-    silversearcher-ag \
-    shfmt \
-    swaks \
-    tig \
-    tmux \
-    traceroute \
-    tree \
-    universal-ctags \
-    xclip \
-    xmlstarlet \
-    yamllint \
-    zeal \
-    zsh
+./install-various-packages.sh
 
 sudo snap install --classic code
 sudo snap install --classic nvim
