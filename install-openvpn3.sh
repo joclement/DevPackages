cc711f7af12e488ea057220cfdc884d2c8de1ebb
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-23 08:55:39 +0200

    refactor: prefer long options
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-openvpn3.sh
+++ install-openvpn3.sh
@@ -2,11 +2,12 @@
 
 set -euo pipefail
 
-curl -fsSL https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub \
+curl --fail --silent --show-error --location \
+    https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub \
     | gpg --dearmor > /tmp/openvpn-repo-pkg-keyring.gpg
 sudo mv /tmp/openvpn-repo-pkg-keyring.gpg /etc/apt/trusted.gpg.d/.
 echo "deb [arch=amd64] https://swupdate.openvpn.net/community/openvpn3/repos jammy main" \
     > /tmp/openvpn3.list
 sudo mv /tmp/openvpn3.list /etc/apt/sources.list.d/openvpn3.list
 sudo apt-get update
-sudo apt-get install -y openvpn3
+sudo apt-get install --yes openvpn3
