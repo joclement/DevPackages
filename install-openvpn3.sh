b447fb831ecbaff85085b16c261bb74deb04ae62
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-23 08:54:26 +0200

    style: spacing for readability
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-openvpn3.sh
+++ install-openvpn3.sh
@@ -9,5 +9,6 @@ sudo mv /tmp/openvpn-repo-pkg-keyring.gpg /etc/apt/trusted.gpg.d/.
 echo "deb [arch=amd64] https://swupdate.openvpn.net/community/openvpn3/repos jammy main" \
     > /tmp/openvpn3.list
 sudo mv /tmp/openvpn3.list /etc/apt/sources.list.d/openvpn3.list
+
 sudo apt-get update
 sudo apt-get install --yes openvpn3
