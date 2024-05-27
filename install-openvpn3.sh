182220092722a90765298852a35218c4aff92504
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-27 17:51:39 +0200

    feat: make openvpn3 installation compatible to Ubuntu24
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-openvpn3.sh
+++ install-openvpn3.sh
@@ -6,8 +6,19 @@ curl --fail --silent --show-error --location \
     https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub \
     | gpg --dearmor > /tmp/openvpn-repo-pkg-keyring.gpg
 sudo mv /tmp/openvpn-repo-pkg-keyring.gpg /etc/apt/trusted.gpg.d/.
-echo "deb [arch=amd64] https://swupdate.openvpn.net/community/openvpn3/repos jammy main" \
-    > /tmp/openvpn3.list
+
+os_version=$(lsb_release -sr)
+openvpn_repo="deb [arch=amd64] https://swupdate.openvpn.net/community/openvpn3/repos"
+if [[ $os_version == "22.04" ]]; then
+    openvpn_repo="$openvpn_repo jammy main"
+elif [[ $os_version == "24.04" ]]; then
+    openvpn_repo="$openvpn_repo noble main"
+else
+    echo "OS version $os_version is unsupported"
+    exit 1
+fi
+
+echo "$openvpn_repo" > /tmp/openvpn3.list
 sudo mv /tmp/openvpn3.list /etc/apt/sources.list.d/openvpn3.list
 
 sudo apt-get update
