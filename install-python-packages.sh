c501b1e9b4a507cbca59d5168a9f3324a4d85f89
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-31 17:47:37 +0200

    docs: add todo
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-python-packages.sh
+++ install-python-packages.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+# TODO avoid `PIP_BREAK_SYSTEM_PACKAGES=1`, see #157
 PIP_BREAK_SYSTEM_PACKAGES=1 pip install --user --requirement ./requirements.txt
 
 xargs -L1 pipx install --force < requirements_pipx.txt
