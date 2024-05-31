7c1b33bb379407beca8d37419ecfdf78667b5a45
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-31 17:44:41 +0200

    refactor: limit scope of env variable
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-python-packages.sh
+++ install-python-packages.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-export PIP_BREAK_SYSTEM_PACKAGES=1
-pip install --user --requirement ./requirements.txt
+PIP_BREAK_SYSTEM_PACKAGES=1 pip install --user --requirement ./requirements.txt
 
 xargs -L1 pipx install --force < requirements_pipx.txt
 pipx inject --pip-args=--constraint=requirements_inject.txt nox nox-poetry
