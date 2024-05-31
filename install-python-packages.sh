0351f71d17ff6cea312be2fe2022e67f01fa19d8
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-05-31 17:55:29 +0200

    refactor: have Python installation more modular
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-python-packages.sh
+++ install-python-packages.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+sudo apt-get install --yes \
+    python-is-python3 \
+    python3 \
+    python3-doc \
+    python3-pip \
+    python3-venv \
+
 # TODO avoid `PIP_BREAK_SYSTEM_PACKAGES=1`, see #157
 PIP_BREAK_SYSTEM_PACKAGES=1 pip install --user --requirement ./requirements.txt
 
