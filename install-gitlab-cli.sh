58b56aa3f2b27373ce4bfda0611a4306f499e921
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-10-14 19:19:45 +0200

    refactor: reduce redundancy
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-gitlab-cli.sh
+++ install-gitlab-cli.sh
@@ -14,10 +14,11 @@ if [[ ${VERSION_EXISTING:-} == "$VERSION" ]]; then
 fi
 
 GITLAB_URL=https://gitlab.com/gitlab-org/cli/-/releases
-GLAB_URL="$GITLAB_URL/v${VERSION}/downloads/glab_${VERSION}_linux_amd64.deb"
+GLAB_PKG_FILENAME="glab_${VERSION}_linux_amd64.deb"
+GLAB_URL="$GITLAB_URL/v$VERSION/downloads/${GLAB_PKG_FILENAME}"
 
 echo "Download glab from $GLAB_URL"
 curl --fail --silent --show-error --location \
     --output-dir /tmp \
     --remote-name "$GLAB_URL"
-sudo apt-get install -f /tmp/glab_${VERSION}_linux_x86_64.deb
+sudo apt-get install -f /tmp/${GLAB_PKG_FILENAME}
