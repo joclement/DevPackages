7ee28e6d2c16e28a5731b1de2079e678915a13be
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-10-18 08:34:35 +0200

    refactor: avoid `PIP_BREAK_SYSTEM_PACKAGES`
    Close #157.
    
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-python-packages.sh
+++ install-python-packages.sh
@@ -3,14 +3,12 @@
 set -euo pipefail
 
 sudo apt-get install --yes \
+    pipx \
     python-is-python3 \
     python3 \
     python3-doc \
     python3-pip \
     python3-venv
-
-# TODO avoid `PIP_BREAK_SYSTEM_PACKAGES=1`, see #157
-PIP_BREAK_SYSTEM_PACKAGES=1 pip install --user --requirement ./requirements.txt
 
 xargs -L1 pipx install --force < requirements_pipx.txt
 pipx inject \
