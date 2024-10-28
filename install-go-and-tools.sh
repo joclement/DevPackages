eaeb483ee074ed4c61a6adb8e3b0bfe96fca0944
Joris Clement <7713214+joclement@users.noreply.github.com>
2024-10-28 20:39:23 +0100

    feat: install a linters runner for golang
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- install-go-and-tools.sh
+++ install-go-and-tools.sh
@@ -12,5 +12,7 @@ sudo update-alternatives --install \
     /usr/local/bin/gofmt gofmt \
     /usr/lib/go-1.21/bin/gofmt 100
 
+sudo snap install --classic golangci-lint
+
 go install github.com/go-critic/go-critic/cmd/gocritic@latest
 go install golang.org/x/tools/cmd/goimports@latest
