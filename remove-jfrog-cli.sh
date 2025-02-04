3bcb0bba3addf445cddb5d7418709506368a1100
Joris Clement <7713214+joclement@users.noreply.github.com>
2025-02-04 14:13:05 +0100

    refactor: jfrog has already been removed a while ago
    Signed-off-by: Joris Clement <7713214+joclement@users.noreply.github.com>

--- remove-jfrog-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-sudo rm -f /etc/apt/keyrings/jfrog-cli-v2-jf.gpg
-sudo rm -f /etc/apt/sources.list.d/jfrog-cli-v2-jf.list
-sudo apt-get purge --yes jfrog-cli-v2-jf || true
