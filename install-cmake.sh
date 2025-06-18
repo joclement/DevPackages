c26345e5fd5c2cf787291749dafae817f250be6c
Joris Clement <7713214+joclement@users.noreply.github.com>
2025-06-18 10:27:23 +0200

    feat!: I no longer need the newest CMake version

--- install-cmake.sh
+++ install-cmake.sh
@@ -2,16 +2,9 @@
 
 set -euo pipefail
 
-kitware_gpg_key=/usr/share/keyrings/kitware-archive-keyring.gpg
-
-kitware_key_url=https://apt.kitware.com/keys/kitware-archive-latest.asc
-test -f /usr/share/doc/kitware-archive-keyring/copyright \
-    || wget --output-document=- $kitware_key_url 2> /dev/null \
-    | gpg --dearmor - \
-        | sudo tee $kitware_gpg_key > /dev/null
-
-echo "deb [signed-by=$kitware_gpg_key] https://apt.kitware.com/ubuntu/ jammy main" \
-    | sudo tee /etc/apt/sources.list.d/kitware.list > /dev/null
+sudo apt-get purge --yes cmake cmake-qt-gui kitware-archive-keyring || true
+sudo rm -f /usr/share/keyrings/kitware-archive-keyring.gpg \
+    /etc/apt/sources.list.d/kitware.list
 
 sudo apt-get update
-sudo apt-get install --yes cmake cmake-qt-gui kitware-archive-keyring
+sudo apt-get install --yes cmake
