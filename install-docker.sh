b4cdf34721657b7defa029d8c0e6b2f6bd9797c8
Joris Clement <7713214+joclement@users.noreply.github.com>
2026-05-26 21:07:47 +0200

    feat: install package to securely store Docker credentials
    [no ci]

--- install-docker.sh
+++ install-docker.sh
@@ -7,7 +7,8 @@ sudo apt-get purge --yes containerd.io || true
 sudo apt-get install --yes \
     docker-buildx \
     docker-compose-v2 \
-    docker.io
+    docker.io \
+    golang-docker-credential-helpers
 
 sudo usermod --append --groups docker "$(whoami)"
 
