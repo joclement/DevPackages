1eef8ee5afddb1055f40182326b253e1db9c473e
Joris Clement <7713214+joclement@users.noreply.github.com>
2026-05-27 13:37:02 +0200

    feat: install Go 1.26
    [no ci]

--- install-go-and-tools.sh
+++ install-go-and-tools.sh
@@ -2,20 +2,10 @@
 
 set -euo pipefail
 
-GO_VERSION="1.23"
-readonly GO_VERSION
-
-sudo apt-get install --yes golang-$GO_VERSION
-
 sudo update-alternatives --remove-all go || true
 sudo update-alternatives --remove-all gofmt || true
 
-sudo update-alternatives --install \
-    /usr/local/bin/go go \
-    /usr/lib/go-$GO_VERSION/bin/go 100
-sudo update-alternatives --install \
-    /usr/local/bin/gofmt gofmt \
-    /usr/lib/go-$GO_VERSION/bin/gofmt 100
+sudo snap install --classic go
 
 sudo snap install --classic golangci-lint
 
